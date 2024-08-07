0aa78e7406d8dacb25617efa9465bf614135be7c
weiqun <1348172474@qq.com>
2024-08-07 17:40:12 +0800

    修改文件导入路径

--- examples/chat/main.py
+++ examples/chat/main.py
@@ -1,3 +1,7 @@
-from simpleaichat import AIChat
+
+import sys
+sys.path.append("..")
+from simpleaichat.simpleaichat import *
+
 
 AIChat(api_key="sk-Ee1imTXK7hDwDM1aFa0337029aD8421bA27882E038CbA163")
