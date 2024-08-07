0f38df617f29dc1a4a75dead59acf36ba8025bb7
Anthonybuer182 <1348172474@qq.com>
2024-08-07 19:44:45 +0800

    添加阿里的api

--- simpleaichat/chatgpt.py
+++ simpleaichat/chatgpt.py
@@ -15,6 +15,13 @@ tool_prompt = """From the list of tools below:
 
 
 class ChatGPTSession(ChatSession):
+    '''
+        https://xiaoai.plus/v1/chat/completions
+        sk-Ee1imTXK7hDwDM1aFa0337029aD8421bA27882E038CbA163
+        
+        https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions
+        sk-1226bc6e75f94b3cba8d8c81dcc8d6f3
+    '''
     api_url: HttpUrl = "https://xiaoai.plus/v1/chat/completions"
     input_fields: Set[str] = {"role", "content", "name"}
     system: str = "You are a helpful assistant."
