693cbf0bbebdf174788938e3ae3d9b863cc85f85
Anthonybuer182 <1348172474@qq.com>
2024-08-09 07:30:10 +0800

    修改内容调用方式

--- examples/chat/main.py
+++ examples/chat/main.py
@@ -3,5 +3,7 @@ import sys
 sys.path.append("..")
 from simpleaichat.simpleaichat import *
 
-
-AIChat(api_key="sk-1226bc6e75f94b3cba8d8c81dcc8d6f3")
+params = {"temperature": 0.0}
+model = "gpt-3.5-turbo-0125"
+# AIChat(api_key="sk-1226bc6e75f94b3cba8d8c81dcc8d6f3")
+ai = AIChat(api_key="sk-1226bc6e75f94b3cba8d8c81dcc8d6f3", console=True, params=params, model=model)
