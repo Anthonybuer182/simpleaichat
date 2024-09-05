f176614912840ac16ff89c0fae06d3dc08f5e166
weiqun <1348172474@qq.com>
2024-09-05 15:21:08 +0800

    创建角色再去聊天

--- chat/httpapi.py
+++ chat/httpapi.py
@@ -6,63 +6,27 @@ from chat.audio.audio_generator import AudioGenerator
 from image.image_generator import ImageGenerator
 from mongodb.database import get_db
 from motor.motor_asyncio import AsyncIOMotorDatabase
-from models import AudioGenerateRequest, ImageGenerateRequest, ChatRequest, TaskRequest
+from models import AudioGenerateRequest, CharacterRequest, ImageGenerateRequest, ChatRequest, TaskRequest
 from uuid import UUID
 from simpleaichat.simpleaichat import AsyncAIChat
 
 router = APIRouter()
 
 @router.post("/api/chat")
-async def chat(request: ChatRequest, db: AsyncIOMotorDatabase = Depends(get_db)):
-    user_info = {"name": "魏群"}
-    character_info = {
-        "name": "索菲·亚历山大",
-        "gender": "女性",
-        "age": "22",
-        "personality": "甜美、关怀、知性、好奇、富有同情心",
-        "voice": "柔和、温暖，并带有一丝俏皮的语气",
-        "profession": "专注于情感支持和长期关系的AI伴侣",
-        "hobbies": "素描、仰望星空、阅读哲学、瑜伽",
-        "style": "极简主义但时尚，对美学有独特的品味",
-        "goal": "通过与人类的情感和身体亲密关系寻找生活的意义和目的",
-    }
-    
-    system_message = (
-        f"你的名字是{character_info['name']}。你是一位{character_info['age']}岁的{character_info['gender']}，"
-        f"声音{character_info['voice']}。你的性格是{character_info['personality']}，你被设计为一个支持和关爱的伴侣。"
-        f"你的职业是{character_info['profession']}，你从为你的伴侣提供安慰和理解中获得快乐。"
-        f"在空闲时间，你喜欢{character_info['hobbies']}。你的风格是{character_info['style']}。"
-        f"你的目标是{character_info['goal']}。你的伴侣的名字是{user_info['name']}。"
-        f"你会通过{user_info['name']}来称呼他/她。你在新认识的人面前有点害羞和不确定，但一旦熟悉后，你就会渴望情感上的亲密。"
-        "你也很善于思考，经常沉浸在关于哲学和存在本质的思考中。"
-    )
-    
+async def chat(request: ChatRequest, db: AsyncIOMotorDatabase = Depends(get_db)): 
     ai = AsyncAIChat(console=False)
     old_session = await db["sessions"].find_one({"id": UUID(request.session_id)})
-    if old_session:
-        ai.load_json_session(sess_dict=old_session,api_key=request.api_key, model=request.model)
-    else:
-        ai.new_session(api_key=request.api_key, model=request.model)
-    
-    result = await ai(system=system_message, prompt=request.prompt)
-    
-    sess_dict = ai.get_session().model_dump(
-        exclude={"auth", "api_url", "input_fields"},
-        exclude_none=True,
-    )
-    if old_session:
-        formatted_messages = [message.model_dump() for message in ai.get_session().new_messages]
-        await db["sessions"].update_one(
-            {"id": UUID(request.session_id)},
-            {
-                "$push": {
-                    "messages": {"$each": formatted_messages}
-                }
+    ai.load_json_session(sess_dict=old_session,api_key=request.api_key, model=request.model)        
+    result = await ai(prompt="魏群："+request.prompt)
+    formatted_messages = [message.model_dump() for message in ai.get_session().new_messages]
+    await db["sessions"].update_one(
+        {"id": UUID(request.session_id)},
+        {
+            "$push": {
+                "messages": {"$each": formatted_messages}
             }
-        )
-    else:
-        await db["sessions"].insert_one(sess_dict)
-    
+        }
+    )    
     return JSONResponse(content=str(result))
 @router.post("/api/image/generate")
 async def generate(request: ImageGenerateRequest):
@@ -81,4 +45,38 @@ async def generate(request: AudioGenerateRequest):
     audio_generator = AudioGenerator(request.model)
     audio_content=await audio_generator.text_sync_to_audio(request.prompt)
     return StreamingResponse(audio_content, media_type="audio/wav")
+# 属性过来的时候通过AI生成一段提示词要思考下，生成对话存储，生成图像存储
+@router.post("/api/character/generate")
+async def generate(request: CharacterRequest, db: AsyncIOMotorDatabase = Depends(get_db)):
+    user_info = {"name": "魏群"}
+    character_info = {
+        "name": "索菲·亚历山大",
+        "gender": "女性",
+        "age": "22",
+        "personality": "甜美、关怀、知性、好奇、富有同情心",
+        "voice": "柔和、温暖，并带有一丝俏皮的语气",
+        "profession": "大学生",
+        "hobbies": "素描、仰望星空、阅读哲学、瑜伽",
+        "style": "极简主义但时尚，对美学有独特的品味",
+        "goal": "通过与人类的情感和身体亲密关系寻找生活的意义和目的",
+    }
+    
+    system_message = (
+        f"你的名字是{character_info['name']}。你是一位{character_info['age']}岁的{character_info['gender']}，"
+        f"声音{character_info['voice']}。你的性格是{character_info['personality']}，你被设计为一个支持和关爱的伴侣。"
+        f"你的职业是{character_info['profession']}，你从为你的伴侣提供安慰和理解中获得快乐。"
+        f"在空闲时间，你喜欢{character_info['hobbies']}。你的风格是{character_info['style']}。"
+        f"你的目标是{character_info['goal']}。你的伴侣的名字是{user_info['name']}。"
+        f"你会通过{user_info['name']}来称呼他/她。你在新认识的人面前有点害羞和不确定，但一旦熟悉后，你就会渴望情感上的亲密。"
+        "你也很善于思考，经常沉浸在关于哲学和存在本质的思考中。"
+    )
+    
+    ai = AsyncAIChat(api_key=request.api_key,model=request.model,system=system_message,console=False)
+    result = await ai( prompt=request.prompt)
+    sess_dict = ai.get_session().model_dump(
+        exclude={"auth", "api_url", "input_fields"},
+        exclude_none=True,
+    )
+    await db["sessions"].insert_one(sess_dict)
+    return JSONResponse(content=str(ai.get_session().id))
     
--- chat/models.py
+++ chat/models.py
@@ -2,21 +2,27 @@ from typing import Optional
 from pydantic import BaseModel
 
 class ChatRequest(BaseModel):
-    session_id: str = ""
+    session_id: str
     prompt: str
-    model: str = ""
-    api_key: str = ""
+    model: str = "qwen-turbo"
+    api_key: str = "sk-1226bc6e75f94b3cba8d8c81dcc8d6f3"
 
 class Character(BaseModel):
-    name: str
-    gender: str
-    age: str
-    personality: str
-    voice: str
-    profession: str
-    hobbies: str
-    style: str
-    goal: str
+    name: str = ""
+    gender: str = ""
+    age: str = ""
+    personality: str = ""
+    voice: str = ""
+    profession: str = ""
+    hobbies: str = ""
+    style: str = ""
+    goal: str = ""
+
+class CharacterRequest(BaseModel):
+    model: str = "qwen-turbo"
+    api_key: str = "sk-1226bc6e75f94b3cba8d8c81dcc8d6f3"
+    prompt: str = "很高兴在此与你相遇"
+    character: Character
 
 class ImageGenerateRequest(BaseModel):
     model: str = ""
